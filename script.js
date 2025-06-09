f31b289332d2e0bf9e3314a64a211fe7e8739385
Mani-Manepalli <699manikanta@gmail.com>
2025-06-09 14:13:52 +0530

    commit

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 
-var tl = document.getElementsByClassName("tl");
-var tc = document.getElementsByClassName("tc");
+var tabl = document.getElementsByClassName("tab-links");
+var tabc = document.getElementsByClassName("tab-contents");
 
-function opentab(tabname){
-    for(tablink of tl){
-        tablink.classlist.remove("al");
+function opentab(tabname) {
+    for (tablink of tabl) {
+        tablink.classList.remove("active-link");
     }
-     for(tabcontent of tc){
-        tabcontent.classlist.remove("at");
+    for (tabcontent of tabc) {
+        tabcontent.classList.remove("active-tab");
     }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+
 }
